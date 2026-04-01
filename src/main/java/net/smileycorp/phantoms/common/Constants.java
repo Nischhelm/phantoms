4276c2a3218f2fadd88c9c2ad83978732a838f1e
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-04-01 03:34:10 +0100

    Update Constants.java

--- src/main/java/net/smileycorp/phantoms/common/Constants.java
+++ src/main/java/net/smileycorp/phantoms/common/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public static final String NAME = "Phantoms";
 	public static final String MODID = "phantoms";
 	public static final String VERSION = "1.0.0";
-	public static final String DEPENDENCIES = "required-after:atlaslib@[1.1.9,)";
+	public static final String DEPENDENCIES = "required-after:atlaslib@[1.1.10,)";
 	public static final String PATH = "net.smileycorp.phantoms.";
 	public static final String CLIENT = PATH + "client.ClientProxy";
 	public static final String SERVER = PATH + "common.CommonProxy";
